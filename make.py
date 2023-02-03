379ab7bd0534be3ec86e70241cad6d3c1284367c
fdagt <112229551+fdagt@users.noreply.github.com>
2023-02-04 02:17:48 +0900

    publicをdocsに改名

--- make.py
+++ make.py
@@ -1,9 +1,12 @@
 import htmlmin
 from jinja2 import Environment, FileSystemLoader
 import json
+import os
 import rjsmin
 
 from script.compile import compile
+
+document_root = "docs"
 
 environment = Environment(loader=FileSystemLoader("template/"))
 
@@ -15,7 +18,7 @@ for i in range(0, len(languages)):
     
 index_template = environment.get_template("index.html")
 index_src = htmlmin.minify(index_template.render(languages=languages))
-with open("public/index.html", mode="w", encoding="utf-8") as index_file:
+with open(os.path.join(document_root, "index.html"), mode="w", encoding="utf-8") as index_file:
     index_file.write(index_src)
 index_template = index_src = None
 
@@ -26,12 +29,12 @@ converters += "]"
     
 script_template = environment.get_template("script.js")
 script_src = rjsmin.jsmin(script_template.render(converter_functions=converters))
-with open("public/script.js", mode="w", encoding="utf-8") as script_file:
+with open(os.path.join(document_root, "script.js"), mode="w", encoding="utf-8") as script_file:
     script_file.write(script_src)
 script_template = script_src = None
 
 test_template = environment.get_template("test.html")
 test_src = htmlmin.minify(test_template.render())
-with open("public/test.html", mode="w", encoding="utf-8") as test_file:
+with open(os.path.join(document_root, "test.html"), mode="w", encoding="utf-8") as test_file:
     test_file.write(test_src)
 test_template = test_src = None
